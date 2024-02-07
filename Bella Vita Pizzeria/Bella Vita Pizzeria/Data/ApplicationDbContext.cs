31b4f56a9a07a13c64a80de54590d140ec347f6e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-07 13:03:50 +0000

    Create project with layers

---
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bella_Vita_Pizzeria.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bella_Vita_Pizzeria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
