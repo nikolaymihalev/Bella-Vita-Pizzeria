7699e87a46c2861acb773b6c2ba8a61b13f49274
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-07 20:10:48 +0000

    Added image and comments to pasta entity

--- Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/Models/Pasta.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/Models/Pasta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,21 +8,31 @@ using System.Threading.Tasks;
 
 namespace BellaVitaPizzeria.Infrastructure.Data.Models
 {
+    [Comment("Таблица за паста")]
     public class Pasta
     {
         [Key]
+        [Comment("Идентификатор на пастата")]
         public int Id { get; set; }
 
         [Required]
+        [Comment("Име на пастата")]
         public required string Title { get; set; }
 
         [Required]
+        [Comment("Съставки на паста")]
         public required string Ingredients { get; set; }
 
         [Required]
+        [Comment("Тегло на пастата")]
         public required string Weight { get; set; }
 
         [Required]
+        [Comment("Цена на пастата")]
         public decimal Price { get; set; }
+
+        [Required]
+        [Comment("Снимка на пастата")]
+        public required byte[] Image { get; set; }
     }
 }
