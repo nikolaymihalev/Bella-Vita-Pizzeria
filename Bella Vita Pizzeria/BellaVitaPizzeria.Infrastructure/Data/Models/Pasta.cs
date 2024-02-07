48a1d04742011f21fa8d950c26e94725bdbe5d6e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-07 13:26:38 +0000

    Create entity for pasta

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/Models/Pasta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellaVitaPizzeria.Infrastructure.Data.Models
+{
+    public class Pasta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Title { get; set; }
+
+        [Required]
+        public required string Ingredients { get; set; }
+
+        [Required]
+        public required string Weight { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
