7ad422d5b15855d262c5c72d3607f9a50df3b617
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-20 13:15:15 +0000

    Isolate seed in configuration

--- Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/ApplicationDbContext.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using BellaVitaPizzeria.Infrastructure.Data.Models;
+﻿using BellaVitaPizzeria.Infrastructure.Data.Configuration;
+using BellaVitaPizzeria.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection.Emit;
@@ -23,32 +24,7 @@ namespace Bella_Vita_Pizzeria.Data
 
             builder.Entity<Product>().Property(e => e.Price).HasPrecision(18, 8);
 
-            builder.Entity<Category>()
-                .HasData(new Category()
-                {
-                    Id = 1,
-                    Name = "Pizza"
-                },
-                new Category()
-                {
-                    Id = 2,
-                    Name = "Pasta"
-                },
-                new Category()
-                {
-                    Id = 3,
-                    Name = "Soup"
-                },
-                new Category()
-                {
-                    Id = 4,
-                    Name = "Grill"
-                },
-                new Category()
-                {
-                    Id = 5,
-                    Name = "Dessert"
-                });
+            builder.ApplyConfiguration(new CategoryConfiguration());
 
             base.OnModelCreating(builder);
         }
