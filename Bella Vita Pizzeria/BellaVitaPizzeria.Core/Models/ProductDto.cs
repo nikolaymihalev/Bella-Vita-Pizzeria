75d0cb91257da8e525027fac877100440c7c8c53
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-21 06:40:05 +0000

    Create dto for product

--- Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/ProductDto.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/ProductDto.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BellaVitaPizzeria.Infrastructure.Data.Constants;
+using System.ComponentModel.DataAnnotations;
 
 namespace BellaVitaPizzeria.Core.Models
 {
     public class ProductDto
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(ValidationConstants.ProductTitleMaxLength, 
+            MinimumLength = ValidationConstants.ProductTitleMinLength)]
+        public required string Title { get; set; }
+
+        [Required]
+        [StringLength(ValidationConstants.ProductIngredientsMaxLength,
+            MinimumLength = ValidationConstants.ProductIngredientsMinLength)]
+        public required string Ingredients { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public int Weight { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public decimal PriceForPizzaBig { get; set; }
+
+        public decimal PriceForPizzaFamily { get; set; }
+
+        [Required]
+        public required string ImageUrl { get; set; }
     }
 }
