656945f4cac5e2517b2b3068b7d79c51777c84a5
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-21 06:36:03 +0000

    Create interface for Product Service

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Contracts/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using BellaVitaPizzeria.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellaVitaPizzeria.Core.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+        Task AddAsync(ProductDto product);
+        Task<ProductDto?> GetByIdAsync(int id);
+        Task EditAsync(ProductDto product);
+        Task DeleteAsync(int id);
+    }
+}
