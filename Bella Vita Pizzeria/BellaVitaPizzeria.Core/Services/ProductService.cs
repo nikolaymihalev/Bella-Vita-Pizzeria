4122e5463b598acbe19d4a6f0da2a448b2349de1
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-21 07:05:35 +0000

    Create Product service and solve add operation

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Services/ProductService.cs
@@ -0,0 +1,69 @@
+﻿using Bella_Vita_Pizzeria.Data;
+using BellaVitaPizzeria.Core.Contracts;
+using BellaVitaPizzeria.Core.Models;
+using BellaVitaPizzeria.Infrastructure.Data.Constants;
+using BellaVitaPizzeria.Infrastructure.Data.Models;
+using Microsoft.Extensions.Logging;
+
+namespace BellaVitaPizzeria.Core.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly ApplicationDbContext context;
+        private readonly ILogger logger;
+
+        public ProductService(
+            ApplicationDbContext _context, 
+            ILogger _logger)
+        {
+            context = _context;
+            logger = _logger;
+        }
+
+        public async Task AddAsync(ProductDto product)
+        {
+            var entity = new Product()
+            {
+                Title = product.Title,
+                Ingredients = product.Ingredients,
+                CategoryId = product.CategoryId,
+                Weight = product.Weight,
+                Price = product.Price,
+                PriceForPizzaBig = product.PriceForPizzaBig,
+                PriceForPizzaFamily = product.PriceForPizzaFamily,
+                ImageUrl = product.ImageUrl,
+            };
+
+            try
+            {
+                await context.Products.AddAsync(entity);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "ProductService.AddAsync");
+                throw new ApplicationException(ErrorMessagesConstants.OperationFailedErrorMessage);
+            }
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/Constants/ErrorMessagesConstants.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/Constants/ErrorMessagesConstants.cs
@@ -10,5 +10,7 @@ namespace BellaVitaPizzeria.Infrastructure.Data.Constants
     {
         public const string RequireErrorMessage = "The field {0} is required!";
         public const string StringLengthErrorMessage = "The field {0} is between {2} and {1} characters long!";
+
+        public const string OperationFailedErrorMessage = "Operation failed. Try again!";
     }
 }
