c83b6574e88ae0f8c4bbfcd4c7657a0e4d71af57
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-21 06:55:16 +0000

    Create dto for category and add comments

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/CategoryDto.cs
@@ -0,0 +1,31 @@
+﻿using BellaVitaPizzeria.Infrastructure.Data.Constants;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellaVitaPizzeria.Core.Models
+{
+    /// <summary>
+    /// Обект за прехвърляне на данни за категория
+    /// </summary>
+    public class CategoryDto
+    {
+        /// <summary>
+        /// Идентификатор на категорията
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Име на категорията
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.CategoryNameMaxLength,
+            MinimumLength = ValidationConstants.CategoryNameMinLength,
+            ErrorMessage = ErrorMessagesConstants.StringLengthErrorMessage)]
+        public required string Name { get; set; }
+    }
+}
--- Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/ProductDto.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/ProductDto.cs
@@ -3,35 +3,67 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BellaVitaPizzeria.Core.Models
 {
+    /// <summary>
+    /// Обект за прехвърляне на данни за продукт
+    /// </summary>
     public class ProductDto
     {
+        /// <summary>
+        /// Идентификатор на продукта
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Име на продукта
+        /// </summary>
         [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
         [StringLength(ValidationConstants.ProductTitleMaxLength, 
             MinimumLength = ValidationConstants.ProductTitleMinLength,
             ErrorMessage = ErrorMessagesConstants.StringLengthErrorMessage)]
         public required string Title { get; set; }
 
+        /// <summary>
+        /// Съставки на продукта
+        /// </summary>
         [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
         [StringLength(ValidationConstants.ProductIngredientsMaxLength,
             MinimumLength = ValidationConstants.ProductIngredientsMinLength,
             ErrorMessage = ErrorMessagesConstants.StringLengthErrorMessage)]
         public required string Ingredients { get; set; }
 
+        /// <summary>
+        /// Идентификатор на категорията на продукта
+        /// </summary>
         [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
         public int CategoryId { get; set; }
 
+        /// <summary>
+        /// Тегло на продукта
+        /// </summary>
         public int Weight { get; set; }
 
+        /// <summary>
+        /// Цена на продукта
+        /// </summary>
         [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
         public decimal Price { get; set; }
 
+        /// <summary>
+        /// Цена за продукт(пица) голяма
+        /// </summary>
         public decimal PriceForPizzaBig { get; set; }
 
+        /// <summary>
+        /// Цена за продукт(пица) семейна
+        /// </summary>
         public decimal PriceForPizzaFamily { get; set; }
 
+        /// <summary>
+        /// Снимка на продукта
+        /// </summary>
         [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
         public required string ImageUrl { get; set; }
+
+        public CategoryDto Category { get; set; }
     }
 }
