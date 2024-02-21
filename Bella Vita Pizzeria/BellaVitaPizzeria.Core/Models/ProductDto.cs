dc1c7deea516e2390eab5cdb211c3768c4a3dba8
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-21 06:43:24 +0000

    Add error messages

--- Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/ProductDto.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/ProductDto.cs
@@ -7,29 +7,31 @@ namespace BellaVitaPizzeria.Core.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
         [StringLength(ValidationConstants.ProductTitleMaxLength, 
-            MinimumLength = ValidationConstants.ProductTitleMinLength)]
+            MinimumLength = ValidationConstants.ProductTitleMinLength,
+            ErrorMessage = ErrorMessagesConstants.StringLengthErrorMessage)]
         public required string Title { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
         [StringLength(ValidationConstants.ProductIngredientsMaxLength,
-            MinimumLength = ValidationConstants.ProductIngredientsMinLength)]
+            MinimumLength = ValidationConstants.ProductIngredientsMinLength,
+            ErrorMessage = ErrorMessagesConstants.StringLengthErrorMessage)]
         public required string Ingredients { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
         public int CategoryId { get; set; }
 
         public int Weight { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
         public decimal Price { get; set; }
 
         public decimal PriceForPizzaBig { get; set; }
 
         public decimal PriceForPizzaFamily { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
         public required string ImageUrl { get; set; }
     }
 }
