265695405eadd2ee398576ad19d99ba2a0a90c4b
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-25 13:30:18 +0000

    Clear project/ Starting from the beginning

--- Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/ApplicationDbContext.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,8 +1,5 @@
-﻿using BellaVitaPizzeria.Infrastructure.Data.Configuration;
-using BellaVitaPizzeria.Infrastructure.Data.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection.Emit;
 
 namespace Bella_Vita_Pizzeria.Data
 {
@@ -12,18 +9,9 @@ namespace Bella_Vita_Pizzeria.Data
             : base(options)
         {
         }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<ProductBuyer> ProductsBuyers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.Entity<ProductBuyer>().HasKey(x => new { x.ProductId, x.BuyerId });
-            builder.Entity<ProductBuyer>().HasOne(x => x.Product).WithMany(x => x.ProductsBuyers).OnDelete(DeleteBehavior.Restrict);            
-
-            builder.ApplyConfiguration(new CategoryConfiguration());
-            builder.ApplyConfiguration(new ProductConfiguration());
-
+        {         
             base.OnModelCreating(builder);
         }
     }
