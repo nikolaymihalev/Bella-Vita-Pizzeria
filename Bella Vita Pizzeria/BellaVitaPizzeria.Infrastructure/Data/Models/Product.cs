50b5051168f50a21e270e484c84d116c042dbb4a
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-26 12:35:51 +0000

    Created Product entity

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace BellaVitaPizzeria.Infrastructure.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public byte[] Image { get; set; } = new byte[128];
+        public int CategoryId { get; set; }
+        public double MinimumPrice { get; set; }
+        public double? MiddlePrice { get; set; }
+        public double? MaximumPrice { get; set; }
+        public string MinimumWeight { get; set; } = string.Empty;
+        public string? MiddleWeight { get; set; }
+        public string? MaxmimumWeight { get; set; }
+    }
+}
