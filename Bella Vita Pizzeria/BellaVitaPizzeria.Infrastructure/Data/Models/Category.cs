798e508ae770242e8c5a1e193a071badd1744d63
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-26 12:38:33 +0000

    Created Category entity

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BellaVitaPizzeria.Infrastructure.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public IEnumerable<Product> Products { get; set; } = new List<Product>();
+    }
+}
--- Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/Models/Product.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/Models/Product.cs
@@ -1,17 +1,39 @@
-﻿namespace BellaVitaPizzeria.Infrastructure.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BellaVitaPizzeria.Infrastructure.Data.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; } = string.Empty;
+
         public string? Description { get; set; }
+
+        [Required]
         public byte[] Image { get; set; } = new byte[128];
+
+        [Required]
         public int CategoryId { get; set; }
+
+        [Required]
         public double MinimumPrice { get; set; }
+
         public double? MiddlePrice { get; set; }
+
         public double? MaximumPrice { get; set; }
+
+        [Required]
         public string MinimumWeight { get; set; } = string.Empty;
+
         public string? MiddleWeight { get; set; }
+
         public string? MaxmimumWeight { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
     }
 }
