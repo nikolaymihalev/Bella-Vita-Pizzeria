c59e6b23f3f381ab3501b70f7b82ab6f52aeaa84
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-26 13:00:05 +0000

    Created Favorite Product entity

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Data/Models/FavoriteProduct.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BellaVitaPizzeria.Infrastructure.Data.Models
+{
+    [Comment("User's favorite product")]
+    public class FavoriteProduct
+    {
+        [Comment("Product identifier")]
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; } = null!;
+
+        [Comment("User identifier")]
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; } = null!;
+    }
+}
