d6d958ea0e1339f0265aa81eba391ea7da443cb9
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-27 16:20:51 +0000

    Implement repository methods

--- Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Common/IRepository.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Common/IRepository.cs
@@ -5,7 +5,7 @@
         IQueryable<T> All<T>() where T : class;
         IQueryable<T> AllReadonly<T>() where T : class;
         Task AddAsync<T>(T entity) where T : class;
-        Task<T> GetByIdAsync<T>(object id) where T : class;
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
         Task DeleteAsync<T>(object id) where T : class;
         void Delete<T>(T entity) where T : class;
         void DeleteRange<T>(IEnumerable<T> entitites) where T : class;
--- Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Common/Repository.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Common/Repository.cs
@@ -1,46 +1,73 @@
-﻿
+﻿using Bella_Vita_Pizzeria.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
 namespace BellaVitaPizzeria.Infrastructure.Common
 {
     public class Repository : IRepository
     {
-        public Task AddAsync<T>(T entity) where T : class
+        public DbContext Context { get; set; }
+
+        private DbSet<T> DbSet<T>() where T : class 
         {
-            throw new NotImplementedException();
+            return this.Context.Set<T>();
+        }
+
+        public Repository(ApplicationDbContext _context)
+        {
+            Context = _context;
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
         }
 
         public IQueryable<T> All<T>() where T : class
         {
-            throw new NotImplementedException();
+            return DbSet<T>().AsQueryable();
         }
 
         public IQueryable<T> AllReadonly<T>() where T : class
         {
-            throw new NotImplementedException();
+            return DbSet<T>().AsQueryable().AsNoTracking();
         }
 
         public void Delete<T>(T entity) where T : class
         {
-            throw new NotImplementedException();
+            EntityEntry entry = this.Context.Entry(entity);
+
+            if (entry.State == EntityState.Detached)
+            {
+                this.DbSet<T>().Attach(entity);
+            }
+
+            entry.State = EntityState.Deleted;
         }
 
-        public Task DeleteAsync<T>(object id) where T : class
+        public async Task DeleteAsync<T>(object id) where T : class
         {
-            throw new NotImplementedException();
+            T? entity = await GetByIdAsync<T>(id);
+
+            if (entity != null) 
+            {
+                DbSet<T>().Remove(entity);
+            }
         }
 
         public void DeleteRange<T>(IEnumerable<T> entitites) where T : class
         {
-            throw new NotImplementedException();
+            DbSet<T>().RemoveRange(entitites);
         }
 
-        public Task<T> GetByIdAsync<T>(object id) where T : class
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
         {
-            throw new NotImplementedException();
+            return await DbSet<T>().FindAsync(id);
         }
 
-        public Task<int> SaveChangesAsync()
+        public async Task<int> SaveChangesAsync()
         {
-            throw new NotImplementedException();
+            return await this.Context.SaveChangesAsync();
         }
     }
 }
