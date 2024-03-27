97b6c447ec533aa7dbb1db49cd371288e9011272
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-27 16:10:24 +0000

    Added methods to repository interface

--- Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Common/IRepository.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Common/IRepository.cs
@@ -2,5 +2,13 @@
 {
     public interface IRepository
     {
+        IQueryable<T> All<T>() where T : class;
+        IQueryable<T> AllReadonly<T>() where T : class;
+        Task AddAsync<T>(T entity) where T : class;
+        Task<T> GetByIdAsync<T>(object id) where T : class;
+        Task DeleteAsync<T>(object id) where T : class;
+        void Delete<T>(T entity) where T : class;
+        void DeleteRange<T>(IEnumerable<T> entitites) where T : class;
+        Task<int> SaveChangesAsync();
     }
 }
--- Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Common/Repository.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Infrastructure/Common/Repository.cs
@@ -1,6 +1,46 @@
-﻿namespace BellaVitaPizzeria.Infrastructure.Common
+﻿
+namespace BellaVitaPizzeria.Infrastructure.Common
 {
     public class Repository : IRepository
     {
+        public Task AddAsync<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> AllReadonly<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync<T>(object id) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange<T>(IEnumerable<T> entitites) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync<T>(object id) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
