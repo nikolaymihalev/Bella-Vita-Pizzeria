0cc0b27724abb3e912b825f17e1fbc2d9a5ca1ae
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-28 17:51:51 +0000

    Create Category models

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/CategoryFormModel.cs
@@ -0,0 +1,14 @@
+﻿using BellaVitaPizzeria.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace BellaVitaPizzeria.Core.Models
+{
+    public class CategoryFormModel
+    {
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.CategoryNameMaxLength, 
+            MinimumLength = ValidationConstants.CategoryNameMinLength, 
+            ErrorMessage = ErrorMessagesConstants.StringLengthErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
--- Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/CategoryInfoModel.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/CategoryInfoModel.cs
@@ -2,5 +2,14 @@
 {
     public class CategoryInfoModel
     {
+        public CategoryInfoModel(
+            int id,
+            string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
