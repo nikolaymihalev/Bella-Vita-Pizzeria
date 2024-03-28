04b2e3d2d8f99639dd1c12abdf631735e45d7fbc
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-28 17:56:19 +0000

    Added category service methods to interface

--- Bella Vita Pizzeria/BellaVitaPizzeria.Core/Contracts/ICategoryService.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Contracts/ICategoryService.cs
@@ -1,6 +1,13 @@
-﻿namespace BellaVitaPizzeria.Core.Contracts
+﻿using BellaVitaPizzeria.Core.Models;
+
+namespace BellaVitaPizzeria.Core.Contracts
 {
     public interface ICategoryService
     {
+        Task<IEnumerable<CategoryInfoModel>> GetAllCategoriesAsync();
+        Task AddAsync(CategoryFormModel model);
+        Task EditAsync(CategoryFormModel model);
+        Task DeleteAsync(int id);
+        Task<CategoryInfoModel> GetByIdAsync(int id);
     }
 }
