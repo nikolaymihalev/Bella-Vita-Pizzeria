9c8bbbcd9a5bd8a2a705f80afc5cca5b6d90481a
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-29 15:52:23 +0000

    Created favorite product model

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/FavoriteProductInfoModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BellaVitaPizzeria.Core.Models
+{
+    public class FavoriteProductInfoModel
+    {
+        public FavoriteProductInfoModel(
+            int productId,
+            string userId,
+            ProductInfoModel product)
+        {
+            ProductId = productId;
+            UserId = userId;
+            Product = product;
+        }
+        public int ProductId { get; set; }
+        public string UserId { get; set; }
+        public ProductInfoModel Product { get; set; }
+    }
+}
