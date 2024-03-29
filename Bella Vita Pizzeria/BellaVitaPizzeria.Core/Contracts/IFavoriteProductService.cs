f33aa3b57fbc9d1678dc7a8a024336a08189240f
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-29 15:54:33 +0000

    Added favorite product service methods

--- Bella Vita Pizzeria/BellaVitaPizzeria.Core/Contracts/IFavoriteProductService.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Contracts/IFavoriteProductService.cs
@@ -1,6 +1,12 @@
-﻿namespace BellaVitaPizzeria.Core.Contracts
+﻿using BellaVitaPizzeria.Core.Models;
+
+namespace BellaVitaPizzeria.Core.Contracts
 {
     public interface IFavoriteProductService
     {
+        Task<IEnumerable<FavoriteProductInfoModel>> GetAllFavoriteProductsAsync();
+        Task<IEnumerable<FavoriteProductInfoModel>> GetAllUserFavoriteProductsAsync();
+        Task AddAsync(FavoriteProductInfoModel model);
+        Task RemoveAsync(int productId, string userId);
     }
 }
