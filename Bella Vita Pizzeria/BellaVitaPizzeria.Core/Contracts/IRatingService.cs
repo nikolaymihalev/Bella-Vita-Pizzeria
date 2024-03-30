bf13e18f7723ed70eb3864184044511ab9faa509
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-30 17:09:10 +0000

    Added rating service methods to interface

--- Bella Vita Pizzeria/BellaVitaPizzeria.Core/Contracts/IRatingService.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Contracts/IRatingService.cs
@@ -1,6 +1,13 @@
-﻿namespace BellaVitaPizzeria.Core.Contracts
+﻿using BellaVitaPizzeria.Core.Models;
+
+namespace BellaVitaPizzeria.Core.Contracts
 {
     public interface IRatingService
     {
+        Task AddAsync(RatingFormModel model);
+        Task EditAsync(RatingFormModel model);
+        Task DeleteAsync(int productId, string userId);
+        Task<double> GetAverageRatingAboutProduct(int productId);
+        Task<IEnumerable<RatingInfoModel>> GetAllRatingsAboutProduct(int productId);
     }
 }
--- Bella Vita Pizzeria/BellaVitaPizzeria.Core/Services/RatingService.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Services/RatingService.cs
@@ -1,8 +1,33 @@
 ﻿using BellaVitaPizzeria.Core.Contracts;
+using BellaVitaPizzeria.Core.Models;
 
 namespace BellaVitaPizzeria.Core.Services
 {
     public class RatingService : IRatingService
     {
+        public Task AddAsync(RatingFormModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int productId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(RatingFormModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<RatingInfoModel>> GetAllRatingsAboutProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<double> GetAverageRatingAboutProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
