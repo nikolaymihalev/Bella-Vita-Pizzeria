99de7ca80156147d6fe6d0aa2e81c5ea9c5c6604
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-07 18:38:02 +0000

    Created product query model

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/ProductQueryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BellaVitaPizzeria.Core.Models
+{
+    public class ProductQueryModel
+    {
+        public IEnumerable<ProductInfoModel> Products { get; set; } = new List<ProductInfoModel>();
+        public int CurrentPage { get; set; }
+        public string? Sorting { get; set; }
+        public string? Category { get; set; }
+        public double PagesCount { get; set; }
+    }
+}
