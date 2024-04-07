c8d607cebc74fff1f8e9ec3bb3910cf1d93aa484
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-07 18:47:36 +0000

    Created product controller with get products operation

---
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bella_Vita_Pizzeria.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+    }
+}
---
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using BellaVitaPizzeria.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bella_Vita_Pizzeria.Controllers
+{
+    public class ProductController : BaseController
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService _productService)
+        {
+            productService = _productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllProducts() 
+        {
+            var model = await productService.GetProductsForPageAsync();
+
+            return View(model);
+        }
+    }
+}
