cfb4cdf9deb08d7d7c197c2052cba1b5825b8ed3
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-08 17:58:21 +0000

    Created Category controller

---
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using BellaVitaPizzeria.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bella_Vita_Pizzeria.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService _categoryService)
+        {
+            categoryService = _categoryService;
+        }
+    }
+}
