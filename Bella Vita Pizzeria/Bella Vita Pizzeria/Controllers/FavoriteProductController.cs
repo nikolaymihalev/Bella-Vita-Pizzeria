6c461484988a06fe013cf642043c14ea06e75ade
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-09 15:18:53 +0000

    Created Favorite Product Controller

---
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/FavoriteProductController.cs
@@ -0,0 +1,15 @@
+﻿using BellaVitaPizzeria.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bella_Vita_Pizzeria.Controllers
+{
+    public class FavoriteProductController : BaseController
+    {
+        private readonly IFavoriteProductService favoriteProductService;
+
+        public FavoriteProductController(IFavoriteProductService _favoriteProductService)
+        {
+            favoriteProductService = _favoriteProductService;
+        }
+    }
+}
