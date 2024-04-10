7dedc6a20c19878fbfea7e45a8ab0757d2a0ce9c
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-10 16:39:53 +0000

    Added view for Get all ratings

--- Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/RatingInfoModel.cs
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/RatingInfoModel.cs
@@ -3,18 +3,21 @@
     public class RatingInfoModel
     {
         public RatingInfoModel(
+            int id,
             int productId,
             string productTitle,
             string userId,
             string username,
             int value)
         {
+            Id = id;
             ProductId = productId;
             ProductTitle = productTitle;
             UserId = userId;
             Username = username;
             Value = value;
         }
+        public int Id { get; set; }
         public int ProductId { get; set; }
         public string ProductTitle { get; set; }
         public string UserId { get; set; }
