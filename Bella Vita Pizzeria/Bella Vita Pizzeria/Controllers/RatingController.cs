2508b2ce02023384ec2856061fb946266c28c2be
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-10 16:42:19 +0000

    Added Add rating method

--- Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/RatingController.cs
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/RatingController.cs
@@ -1,6 +1,8 @@
 ﻿using BellaVitaPizzeria.Core.Contracts;
+using BellaVitaPizzeria.Core.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Bella_Vita_Pizzeria.Controllers
 {
@@ -38,6 +40,54 @@ namespace Bella_Vita_Pizzeria.Controllers
             model.All(x => x.ProductTitle == product.Title);
 
             return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Add(int productId)
+        {
+            var product = await productService.GetByIdAsync(productId);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var model = new RatingFormModel()
+            {
+                ProductId = productId,
+                Value = 0,
+                UserId = User.Id()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(RatingFormModel model, int productId)
+        {
+            model.UserId = User.Id();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var product = await productService.GetByIdAsync(productId);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var products = await ratingService.GetAllRatingsAboutProductAsync(productId);
+
+            if (products.Any(x => x.UserId == User.Id()))
+            {
+                return BadRequest();
+            }
+
+            await ratingService.AddAsync(model);
+
+            return RedirectToAction(nameof(GetAllRatings), new { productId });
         }
     }
 }
