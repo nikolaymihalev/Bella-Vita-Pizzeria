19be9cc66516baf15aef9e6c4ce899e637ee552d
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-10 16:30:36 +0000

    Created Rating Controller

---
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/RatingController.cs
@@ -0,0 +1,15 @@
+﻿using BellaVitaPizzeria.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bella_Vita_Pizzeria.Controllers
+{
+    public class RatingController : BaseController
+    {
+        private readonly IRatingService ratingService;
+
+        public RatingController(IRatingService _ratingService)
+        {
+            ratingService = _ratingService;
+        }
+    }
+}
