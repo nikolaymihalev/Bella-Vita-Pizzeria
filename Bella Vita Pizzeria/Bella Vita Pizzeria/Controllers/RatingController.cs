5b10f43d0c94d74678725e975f1354e9918e04d2
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-10 16:32:58 +0000

    Added Get all ratings method

--- Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/RatingController.cs
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/RatingController.cs
@@ -1,4 +1,5 @@
 ﻿using BellaVitaPizzeria.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bella_Vita_Pizzeria.Controllers
@@ -6,10 +7,37 @@ namespace Bella_Vita_Pizzeria.Controllers
     public class RatingController : BaseController
     {
         private readonly IRatingService ratingService;
+        private readonly IProductService productService;
 
-        public RatingController(IRatingService _ratingService)
+        public RatingController(
+            IRatingService _ratingService, 
+            IProductService _productService)
         {
             ratingService = _ratingService;
+            productService = _productService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAllRatings(int productId)
+        {
+            var product = await productService.GetByIdAsync(productId);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var model = await ratingService.GetAllRatingsAboutProductAsync(productId);
+
+            if (model.Count() == 0)
+            {
+                return RedirectToAction("Details", "Product", new { id = productId });
+            }
+
+            model.All(x => x.ProductTitle == product.Title);
+
+            return View(model);
         }
     }
 }
