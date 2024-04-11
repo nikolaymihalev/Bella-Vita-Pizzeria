e7c0310781382e3e0325b0b11c9954f7399ae12f
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-11 16:55:33 +0000

    Added Add product method

--- Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/ProductController.cs
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/ProductController.cs
@@ -1,16 +1,29 @@
 ﻿using BellaVitaPizzeria.Core.Contracts;
+using BellaVitaPizzeria.Core.Models;
+using BellaVitaPizzeria.Core.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Bella_Vita_Pizzeria.Controllers
 {
     public class ProductController : BaseController
     {
         private readonly IProductService productService;
+        private readonly ICategoryService categoryService;        
+        private readonly IFavoriteProductService favoriteProductService;        
+        private readonly IRatingService ratingService;        
 
-        public ProductController(IProductService _productService)
+        public ProductController(
+            IProductService _productService, 
+            ICategoryService _categoryService,
+            IFavoriteProductService _favoriteProductService,
+            IRatingService _ratingService)
         {
             productService = _productService;
+            categoryService = _categoryService;
+            favoriteProductService = _favoriteProductService;
+            ratingService = _ratingService;
         }
 
         [HttpGet]
@@ -19,6 +32,49 @@ namespace Bella_Vita_Pizzeria.Controllers
         {
             var model = await productService.GetProductsForPageAsync();
 
+            return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = new ProductFormModel()
+            {
+                Categories = await categoryService.GetAllCategoriesAsync(),
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRecipe(ProductFormModel model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                model.Categories = await categoryService.GetAllCategoriesAsync();
+                return View(model);
+            }
+
+            if (model.ImageFile == null)
+            {
+                model.Categories = await categoryService.GetAllCategoriesAsync();
+                return View(model);
+            }
+
+            if (model.ImageFile != null && model.ImageFile.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    model.ImageFile.CopyTo(memoryStream);
+                    byte[] imageBytes = memoryStream.ToArray();
+
+                    model.Image = imageBytes;
+
+                    await productService.AddAsync(model);
+                }
+                return RedirectToAction(nameof(AllProducts));
+            }
+
+            model.Categories = await categoryService.GetAllCategoriesAsync();
             return View(model);
         }
     }
