d84ed816fe83f0b8df56725aa240f457b21311de
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-21 16:04:55 +0000

    Created Admin controller

---
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using BellaVitaPizzeria.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bella_Vita_Pizzeria.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly IProductService productService;
+
+        public AdminController(IProductService _productService)
+        {
+            productService = _productService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
