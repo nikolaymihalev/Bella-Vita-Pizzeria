716c9eae9892b743b60bdfe9fc0725a8f37758f9
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-24 18:55:27 +0000

    Created Statistic model

---
+++ Bella Vita Pizzeria/BellaVitaPizzeria.Core/Models/Cart/OrdersStatisticModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BellaVitaPizzeria.Core.Models.Cart
+{
+    public class OrdersStatisticModel
+    {
+        public Dictionary<int, int> MonthsCounts { get; set; } = new Dictionary<int, int>();
+    }
+}
