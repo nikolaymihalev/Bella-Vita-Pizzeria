3f9d8d4626ab0a5ed23026355d97fbcd78f52270
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-25 12:55:17 +0000

    Updated search form view

--- Bella Vita Pizzeria/Bella Vita Pizzeria/wwwroot/js/site.js
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/wwwroot/js/site.js
@@ -1,9 +1,5 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-function searchForm() {
-    var x = document.getElementById("myDIV");
+﻿function searchForm() {
+    var x = document.getElementById("searchForm");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
