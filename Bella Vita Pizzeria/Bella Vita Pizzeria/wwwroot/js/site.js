b2b479303a3cb2182c36d241ca2a8f0ad14f86cb
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-25 12:22:45 +0000

    Change the search link to button

--- Bella Vita Pizzeria/Bella Vita Pizzeria/wwwroot/js/site.js
+++ Bella Vita Pizzeria/Bella Vita Pizzeria/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function searchForm() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
